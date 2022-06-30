d17091ac50dd82b6131369517ed0c51612118e4d
vincentramirez <vinnie.ramirez@gmail.com>
2022-06-29 22:06:57 -0700

    cp 2

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,6 @@ output "clusterprofile_id" {
   value = spectrocloud_cluster_profile.profile.id
 }
 
-output "clusterprofile_id" {
+output "clusterprofile_id2" {
   value = spectrocloud_cluster_profile.profile2.id
 }
